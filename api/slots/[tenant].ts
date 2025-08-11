d732f7c4b2769a8b72433a33ee652f30325c4a90
fraym-studio <simonlucas6716@gmail.com>
2025-08-11 18:57:54 -0400

    Update [tenant].ts

--- api/slots/[tenant].ts
+++ api/slots/[tenant].ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { Pool } from "pg";
+import pg from "pg";
+const { Pool } = pg;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 type TimeWindow = { start: string; end: string };
