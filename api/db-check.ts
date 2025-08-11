ed89e673f49818c38c76e932476004975368d0c5
fraym-studio <simonlucas6716@gmail.com>
2025-08-11 18:58:45 -0400

    Update db-check.ts

--- api/db-check.ts
+++ api/db-check.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { Pool } from "pg";
+import pg from "pg";
+const { Pool } = pg;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 export default async (_: VercelRequest, res: VercelResponse) => {
